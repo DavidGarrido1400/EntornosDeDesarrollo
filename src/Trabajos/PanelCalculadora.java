0f0c9bf7b380f94575d8294841966d5249920496
David Garrido García <71704387+DavidGarrido1400@users.noreply.github.com>
2021-04-13 09:43:45 +0200

    Modelo ER terminado

--- src/Trabajos/PanelCalculadora.java
+++ src/Trabajos/PanelCalculadora.java
@@ -1,5 +1,147 @@
 package Trabajos;
 
-public class PanelCalculadora {
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class PanelCalculadora extends JPanel {
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Create the panel.
+	 * 
+	 * 
+	 */
+	
+	
+	public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    PanelCalculadora window = new PanelCalculadora();
+                    window.frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+	} 
+	
+
+		
+	
+	public PanelCalculadora() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel lblNum1 = new JLabel("Número 1:");
+		GridBagConstraints gbc_lblNum1 = new GridBagConstraints();
+		gbc_lblNum1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNum1.gridx = 0;
+		gbc_lblNum1.gridy = 1;
+		add(lblNum1, gbc_lblNum1);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.anchor = GridBagConstraints.WEST;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 1;
+		add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JLabel lblNum2 = new JLabel("Número 2:");
+		GridBagConstraints gbc_lblNum2 = new GridBagConstraints();
+		gbc_lblNum2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNum2.gridx = 0;
+		gbc_lblNum2.gridy = 2;
+		add(lblNum2, gbc_lblNum2);
+		
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.anchor = GridBagConstraints.WEST;
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.gridx = 1;
+		gbc_textField_2.gridy = 2;
+		add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblResult = new JLabel("Resultado:");
+		GridBagConstraints gbc_lblResult = new GridBagConstraints();
+		gbc_lblResult.insets = new Insets(0, 0, 5, 5);
+		gbc_lblResult.gridx = 0;
+		gbc_lblResult.gridy = 4;
+		add(lblResult, gbc_lblResult);
+		
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 4;
+		add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
+		
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridwidth = 3;
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 6;
+		add(panel, gbc_panel);
+		
+		JButton btnSumar = new JButton("Sumar");
+		btnSumar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				
+				int n1 , n2 , resultado;
+				n1= Integer.parseInt(textField.getText());
+				n2= Integer.parseInt(textField_1.getText());
+				resultado = n1 + n2 ;
+				textField_2.setText(String.valueOf(resultado));
+			}
+		});
+		panel.add(btnSumar);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				cerrarAplicacion();
+				
+			}
+		});
+		panel.add(btnSalir);
+
+	}
+	
+	private void cerrarAplicacion() {
+		String [] opciones ={"Aceptar","Cancelar"};
+		int eleccion = JOptionPane.showOptionDialog(frame,"¿Desea cerrar la aplicación?","Salir de la aplicación",
+		JOptionPane.YES_NO_OPTION,
+		JOptionPane.QUESTION_MESSAGE, null, opciones, "Aceptar");
+		if (eleccion == JOptionPane.YES_OPTION) {
+			System.exit(0);
+		}
+	
+
+
+}
 }
